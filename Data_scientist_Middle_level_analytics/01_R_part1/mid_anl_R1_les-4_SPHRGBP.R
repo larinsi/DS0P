7cbc1eb7bd68f36012574930fb360cea9342c510
larinsi <larinsi15@gmail.com>
2021-09-10 14:50:00 +0300

    Added Module 1 Lesson 7 materials

---
+++ Data_scientist_Middle_level_analytics/01_R_part1/mid_anl_R1_les-4_SPHRGBP.R
@@ -0,0 +1,27 @@
+### Lesson 4 ###
+
+# basic calculations
+5 ** 2
+5 ^ 2
+
+exp(1)
+exp(2)
+
+log(100, base = 10)
+log(100, 10)
+log(100)
+
+# rounding
+round(1.67)
+round(1.67, 1)
+floor(1.67)
+ceiling(1.67)
+
+# working with numeric variables
+start.sum <- 200
+Years <- 3
+end.sum <- start.sum * 2^Years
+end.sum
+
+
+
