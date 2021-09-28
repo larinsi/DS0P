3a5c83abe995a1a2be6442d19472b64b29e30146
larinsi <larinsi15@gmail.com>
2021-09-28 14:53:38 +0300

    Homework Task 1 done.

--- Data_scientist_Middle_level_analytics/04_Creating_dashboards/tit/tit.R
+++ Data_scientist_Middle_level_analytics/04_Creating_dashboards/tit/tit.R
@@ -9,26 +9,24 @@
 
 library(shiny)
 
-# Define UI for application that draws a histogram
+
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Titanic Passengers"),
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+    # Row with 2 selectInputs 
+    fluidRow(column(6,
+                    selectInput("strparam",
+                                "Barplot",
+                                c(`Survived` = "Survived",
+                                  `Class` = "PClass",
+                                  `Sex` = "Sex"))),
+             column(6,
+                    selectInput("numparam",
+                                "Histplot",
+                                c("Age",
+                                  "Fare")))
     )
 )
 
